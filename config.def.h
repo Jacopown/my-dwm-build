206fb04db72c242c571785f82631187e4a654291
“Jacopown” <jacopomehi@gmail.com>
2021-04-15 10:37:36 +0200

    config changes for windows arrengements and keybindings changes

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const unsigned int gappih = 20;	/* horiz inner gap between windows */
 static const unsigned int gappiv = 10;	/* vert inner gap between windows */
 static const unsigned int gappoh = 10;	/* horiz outer gap between windows and screen edge */
 static const unsigned int gappov = 30;	/* vert outer gap between windows and screen edge */
-static int smartgaps = 0;				/* 1 means no outer gap when there is only one window */
+static int smartgaps = 0;		/* 1 means no outer gap when there is only one window */
 static const int showbar = 1;		/* 0 means no bar */
 static const int topbar = 1;		/* 0 means bottom bar */
 static const char *fonts[] = {"monospace:size=10"};
@@ -46,7 +46,7 @@ static const int resizehints = 1; /* 1 means respect size hints in tiled resizal
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{"[]=",       tile}, /* first entry is default */
+	{"[]=",       tile}, 			/* first entry is default */
 	{"[M]",       monocle},
 	{"[@]",       spiral},
 	{"[\\]",      dwindle},
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 	{":::",       gaplessgrid},
 	{"|M|",       centeredmaster},
 	{">M>",       centeredfloatingmaster},
-	{"><>",       NULL}, /* no layout function means floating behavior */
+	{"><>",       NULL}, 			/* no layout function means floating behavior */
 	{NULL,        NULL},
 };
 
@@ -85,16 +85,16 @@ static const char *browsercmd[] = {"firefox", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{MODKEY,                        XK_d,      spawn,          {.v = dmenucmd}},
+	{MODKEY,                        XK_space,  spawn,          {.v = dmenucmd}},
 	{MODKEY,                        XK_Return, spawn,          {.v = termcmd}},
 	{MODKEY,                        XK_w,      spawn,          {.v = browsercmd}},
 	{MODKEY,                        XK_b,      togglebar,      {0}},
-	{MODKEY,                        XK_j,      focusstack,     {.i = +1}},
-	{MODKEY,                        XK_k,      focusstack,     {.i = -1}},
-	{MODKEY,                        XK_i,      incnmaster,     {.i = +1}},
-	{MODKEY,                        XK_p,      incnmaster,     {.i = -1}},
-	{MODKEY,                        XK_h,      setmfact,       {.f = -0.05}},
-	{MODKEY,                        XK_l,      setmfact,       {.f = +0.05}},
+	{MODKEY,                        XK_l,      focusstack,     {.i = +1}},
+	{MODKEY | ShiftMask,            XK_l,      focusstack,     {.i = -1}},
+	{MODKEY,                        XK_k,      incnmaster,     {.i = +1}},
+	{MODKEY | ShiftMask,            XK_k,      incnmaster,     {.i = -1}},
+	{MODKEY,                        XK_j,      setmfact,       {.f = +0.05}},
+	{MODKEY | ShiftMask,            XK_j,      setmfact,       {.f = -0.05}},
 	{MODKEY | ShiftMask,            XK_Return, zoom,           {0}},
 	{MODKEY | Mod1Mask,             XK_u,      incrgaps,       {.i = +1}},
 	{MODKEY | Mod1Mask | ShiftMask, XK_u,      incrgaps,       {.i = -1}},
@@ -107,10 +107,14 @@ static Key keys[] = {
 	{MODKEY,                        XK_Tab,    view,           {0}},
 	{MODKEY | ShiftMask,            XK_q,      killclient,     {0}},
 	{MODKEY,                        XK_t,      setlayout,      {.v = &layouts[0]}},
-	{MODKEY,                        XK_f,      setlayout,      {.v = &layouts[1]}},
-	{MODKEY,                        XK_m,      setlayout,      {.v = &layouts[2]}},
-	{MODKEY,                        XK_space,  setlayout,      {0}},
-	{MODKEY | ShiftMask,            XK_space,  togglefloating, {0}},
+	{MODKEY | ShiftMask,            XK_t,      setlayout,      {.v = &layouts[2]}},
+	{MODKEY,                        XK_y,      setlayout,      {.v = &layouts[1]}},
+	{MODKEY | ShiftMask,            XK_y,      setlayout,      {.v = &layouts[4]}},
+	{MODKEY,                        XK_u,      setlayout,      {.v = &layouts[7]}},
+	{MODKEY | ShiftMask,            XK_u,      setlayout,      {.v = &layouts[10]}},
+	{MODKEY,                        XK_i,      setlayout,      {.v = &layouts[11]}},
+	{MODKEY | ShiftMask,            XK_i,      setlayout,      {.v = &layouts[5]}},
+	{MODKEY,                        XK_f,      setlayout,      {0}},
 	{MODKEY,                        XK_0,      view,           {.ui = ~0}},
 	{MODKEY | ShiftMask,            XK_0,      tag,            {.ui = ~0}},
 	{MODKEY,                        XK_comma,  focusmon,       {.i = -1}},
