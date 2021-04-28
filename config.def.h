59225e1a679e6fe46a9bd66b96ff1f09ee00b7a4
Jacopown <jacopomehi@gmail.com>
2021-04-28 19:56:41 +0200

    Color scheme of dmenu automatically set from background color palette

--- config.def.h
+++ config.def.h
@@ -84,7 +84,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", NormalBG, "-nf", FontColor, "-sb", SelectedBG, "-sf", FontColor, NULL};
+static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *browsercmd[] = {"firefox", NULL};
 
